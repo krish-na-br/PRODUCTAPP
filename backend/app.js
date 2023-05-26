294ce892a14124d8e615dbbe61713b2c22e74b1d
krish-na-br <krishnabrsivam@gmail.com>
2023-05-26 16:31:48 +0530

    newone

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,34 @@
-const express = require('express'); //to build backend quickly
-const app = express(); //lisence of express
-const cors = require('cors'); //to cross original policies
+const express = require('express'); //! to build backend quickly 
+const app = express(); //!instance of express
+const cors = require('cors'); //!for cross orgin policy
+const PORT = 8746
+app.use(cors()); //! "app.use(cors())" means use this cors package when server starts up
 
-const PORT = 8745;
-require("./dB")
-
-app.use(cors()); //app.use(cors()) means use this cors package when server starts up
-
-
-app.use(express.json()); //app.use(express.json
-app.use(express.urlencoded({ extended: true })); //app.use(express.urlencoded)
-app.use(express.static('public')); //app.use(express.static)
+app.use(express.json()); //! to parse json data
+app.use(express.urlencoded({extended:true})); //! to parse url encoded data
+app.use(express.static('public')); //! to serve static files
 
 
+// DB 
+require('./dB') //!mongoose DB connection folder is called here hen server starts up
+const PRODUCT = require('./model/product')
 
 
+app.post('/product',async (req,res)=>{
+    try {
+        console.log(req.body);
+        let item = req.body;
 
+        const saveItem = PRODUCT(item);
+        await saveItem.save();
+        res.send("Success!");
 
-
+    } catch (error) {
+        res.send(error.message)
+    }
+})
 
 
 app.listen(PORT,()=>{
-    console.log(`server is runnig on port ${PORT}`);
+    console.log(`server is running on port ${PORT}`)
 })
